474f85dfbd5d011304452140270791100ee1d1a2
Denys Levshun <djoni1vincent@djoni1vincent.local>
2026-07-06 18:49:18 +0200

    fead: add Wishlist model with user and product relation

--- wishlist/models.py
+++ wishlist/models.py
@@ -1,3 +1,15 @@
 from django.db import models
 
+from products.models import Product
+from users.models import CustomUser
+
 # Create your models here.
+
+
+class Wishlist(models.Model):
+    user = models.ForeignKey(CustomUser, on_delete=models.CASCADE)
+    products = models.OneToOneField(Product, on_delete=models.CASCADE)
+    created_at = models.DateTimeField(auto_now_add=True)
+
+    class Meta:
+        ordering = ["user"]
