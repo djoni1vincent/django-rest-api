1b75bb16cb8b72af8a98e753c1fa01ddedefad46
Denys Levshun <djoni1vincent@djoni1vincent.local>
2026-07-06 12:24:05 +0200

    fead: create custom command for creating dummy data

---
+++ products/management/commands/create_data.py
@@ -0,0 +1,39 @@
+from django.core.management.base import BaseCommand, CommandError
+
+from products.models import Category, Order, Product, Review
+from users.models import CustomUser
+
+
+class Command(BaseCommand):
+    def handle(self, *args, **options):
+        Product.objects.all().delete()
+        products_to_create = []
+
+        categories = [
+            "tech",
+            "electronics",
+            "home",
+        ]
+        category_objects = [
+            Category.objects.get_or_create(name=name)[0] for name in categories
+        ]
+
+        for i in range(options["count"]):
+            price = i * 10
+            products_to_create.append(
+                Product(
+                    name=f"Item {i}",
+                    price_amount=price,
+                    description="test description",
+                ),
+            )
+
+        Product.objects.bulk_create(products_to_create)
+
+        for i, product in enumerate(products_to_create):
+            product.category.set([category_objects[i % len(category_objects)]])
+
+    def add_arguments(self, parser):
+        parser.add_argument(
+            "--count", type=int, help="Number of products to create", default=5
+        )
