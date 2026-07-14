d1b4619e4d44dbcb66401dec681cf2e0eff3f24f
Denys Levshun <djoni1vincent@djoni1vincent.local>
2026-07-14 23:58:58 +0200

    testing

--- products/serializers.py
+++ products/serializers.py
@@ -5,7 +5,7 @@ from users.serializers import UserSerializer
 from .models import Category, Product, Review
 
 
-# testing
+# t
 class CategorySerializer(serializers.ModelSerializer):
     class Meta:
         model = Category
