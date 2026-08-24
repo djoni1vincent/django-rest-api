7f2356afec2d09fd641f8173e09e3144cc34a325
Denys Levshun <djoni1vincent@djoni1vincent.local>
2026-08-24 21:43:08 +0200

    fix: AssertionError, add basename to wishlist router

--- wishlist/urls.py
+++ wishlist/urls.py
@@ -4,7 +4,7 @@ from rest_framework import routers
 from . import views
 
 router = routers.DefaultRouter()
-router.register(r"wishlist", views.WishlistViewSet)
+router.register(r"wishlist", views.WishlistViewSet, basename="wishlist")
 
 urlpatterns = [
     path("", include(router.urls)),
